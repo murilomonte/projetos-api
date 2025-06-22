031b242163af08b5a9748234ba6f207588365b15
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-06-22 09:13:30 -0300

    feat: alterações nos schemas e implementação de alguns recursos

--- app/router/projects.py
+++ app/router/projects.py
@@ -1,29 +1,82 @@
 from http import HTTPStatus
-# from typing import Any, Union
-from fastapi import APIRouter
-project_router:APIRouter = APIRouter(prefix='/projects')
+from typing import Annotated
+from datetime import datetime
+import uuid
 
-# Obtém todos os projetos
-@project_router.get("/", status_code=HTTPStatus.OK)
-def read_all_projects():
-    return {"Hello": "World"}
+from fastapi import APIRouter, HTTPException, Query
+from app.schemas.schemas import Message, Project, ProjectDB, ProjectList, FilterParams  # type: ignore
 
-# Obtém somente um projeto
-@project_router.get("/{project_id}", status_code=HTTPStatus.OK)
-def read_project():
-    ...
+project_router: APIRouter = APIRouter(prefix="/projects")
 
-# Cria um projeto
-@project_router.post("/", status_code=HTTPStatus.CREATED)
-def create_project():
-    ...
+database: list[ProjectDB] = []
 
-# Atualiza um projeto
-@project_router.put("/{project_id}", status_code=HTTPStatus.OK)
-def update_project():
-    ...
 
-# Deleta um projeto
-@project_router.delete("/{project_id}", status_code=HTTPStatus.OK)
-def delete_project():
-    ...
+# Obtém todos os projetos - falta o filtro
+@project_router.get("/", status_code=HTTPStatus.OK, response_model=ProjectList)
+def read_all_projects(filter_query: Annotated[FilterParams, Query()]):
+    # skip=None limit=None status=None priority=None
+
+    filtered_projects: list[ProjectDB] = [] # type: ignore
+    # percorre todos os itens de database e adiciona ao filtered apenas os que atenderem os requisitos de filter_query
+    return {"projects": database}
+
+
+# Obtém somente um projeto - ok
+@project_router.get("/{project_id}", status_code=HTTPStatus.OK, response_model=ProjectDB)
+def read_project(project_id: uuid.UUID):
+    ## TODO: tentar fazer isso da melhor forma
+    for project in database:
+        if project.id == project_id:
+            return project
+    raise HTTPException(
+        status_code=HTTPStatus.NOT_FOUND, detail='Project not found'
+    )
+
+
+# Cria um projeto - ok
+@project_router.post("/", status_code=HTTPStatus.CREATED, response_model=ProjectDB)
+def create_project(project: Project):
+    new_project = ProjectDB(
+        id=uuid.uuid4(),
+        title=project.title,
+        description=project.description,
+        priority=project.priority,
+        status=project.status,
+        created_at=datetime.now()
+    )
+
+    database.append(new_project)
+    ## https://stackoverflow.com/questions/19199872/best-practice-for-restful-post-response
+    return new_project
+
+
+# Atualiza um projeto - ok
+@project_router.put("/{project_id}", status_code=HTTPStatus.OK, response_model=ProjectDB)
+def update_project(project_id: uuid.UUID):
+    ## TODO: tentar fazer isso da melhor forma
+    for index, project in enumerate(database):
+        if project.id == project_id:
+            return project
+
+    raise HTTPException(
+        status_code=HTTPStatus.NOT_FOUND, detail='Project not found'
+    )
+
+
+# Deleta um projeto - ok
+@project_router.delete("/{project_id}", status_code=HTTPStatus.NO_CONTENT)
+def delete_project(project_id: uuid.UUID):
+    ## TODO: tentar fazer isso da melhor forma
+    # https://stackoverflow.com/questions/25970523/restful-what-should-a-delete-response-body-contain
+    found: bool = False
+    for index, project in enumerate(database):
+        if project.id == project_id:
+            database.pop(index)
+            found = True
+            break
+
+    if not found:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Project not found'
+        )
+        
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,11 +1,15 @@
 from typing import Literal, Optional
 from pydantic import BaseModel
+from datetime import datetime
+import uuid
 
 ## TODO: Buscar sobre o Field do pydantic
 # https://fastapi.tiangolo.com/tutorial/query-param-models/#query-parameters-with-a-pydantic-model
 
+
 class Message(BaseModel):
     message: str
+
 
 class Project(BaseModel):
     title: str
@@ -13,14 +17,18 @@ class Project(BaseModel):
     priority: Literal[1, 2, 3]
     status: Literal["Planejado", "Em Andamento", "Concluído", "Cancelado"]
 
+
 class ProjectDB(Project):
-    id: int
+    id: uuid.UUID
+    created_at: datetime
+
 
 class ProjectList(BaseModel):
     projects: list[ProjectDB]
 
+
 class FilterParams(BaseModel):
-    skip: Optional[int] = None # Número de registros a pular (para paginação).
-    limit: Optional[int] = None # Número máximo de registros a retornar.
-    status: Optional[Literal["Planejado", "Em Andamento", "Concluído", "Cancelado"]] = None # Filtrar por status do projeto.
+    skip: Optional[int] = None  # Número de registros a pular (para paginação).
+    limit: Optional[int] = None  # Número máximo de registros a retornar.
+    status: Optional[Literal["Planejado", "Em Andamento", "Concluído", "Cancelado"]] = None # Filtrar por status do projeto.)
     priority: Optional[Literal[1, 2, 3]] = None  # Filtrar por prioridade
